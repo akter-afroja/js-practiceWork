a7809e45781d97bc76945742ae96254152d1ecb3
akter-afroja <afakter08@gmail.com>
2022-08-03 00:37:39 +0600

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 import './style.css';
 
 // Write Javascript code!
+
+console.log('Hello dummy');
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Hello World! so obvious!!!</h1>`;
