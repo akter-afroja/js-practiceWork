b94f55a2ba96245df7aec587cdc0f3decb548a41
akter-afroja <afakter08@gmail.com>
2022-08-03 00:52:11 +0600

    1 file modified

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ import './style.css';
 
 console.log('Hello dummy');
 
+const syncWithCallback = () => {
+  for (let index = 0; index < 10; index++) {
+    console.log('rocking:\t', index,'\t', new Date());
+  }
+}
+
+syncWithCallback();
+console.log('all is well');
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Hello World! so obvious!!!</h1>`;
